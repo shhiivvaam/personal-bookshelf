dfdf7a6bba133be4ed83799c6d2b346bba355528
Shivam Kumar <life.shivam2394@gmail.com>
2024-06-09 01:10:58 +0530

    complted feature functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// importing pages
+import Home from './pages/Home';
+import BookShelf from './pages/BookShelf';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/bookshelf" element={<BookShelf />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
